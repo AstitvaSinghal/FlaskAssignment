2af8c98d05afe3194de37d6dc9bc54cd52a221d2
singhalswastik <swastik@iitk.ac.in>
2021-03-09 00:16:52 +0530

    changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 from requests import get
 
 app = Flask(__name__,static_url_path='/static')
@@ -51,6 +51,56 @@ def airing_today():
         except:
             first_air_date="N/A"
         results.append({"name":name,"backdrop_path":backdrop_path,"first_air_date":first_air_date})
+    return {"results":results}
+
+@app.route('/search_movie')
+def search_movie():
+    search_query=request.args.get('search_query')
+    response=get(f'{SITE_NAME}search/movie?{API_KEY}&query={search_query}&language=en-US&page=1&include_adult=false')
+    response=response.json()
+    # return response
+    results=[]
+    for i in range(0,len(response['results'])):
+        try:
+            id=response['results'][i]['id']
+        except:
+            id="N/A"
+        try:
+            title=response['results'][i]['title']
+        except:
+            title="N/A"
+        try:
+            overview=response['results'][i]['overview']
+        except:
+            overview="N/A"
+        try:
+            poster_path=response['results'][i]['poster_path']
+        except:
+            poster_path="N/A"
+        try:
+            release_date=response['results'][i]['release_date']
+        except:
+            release_date="N/A"
+        try:
+            vote_average=response['results'][i]['vote_average']
+        except:
+            vote_average="N/A"
+        try:
+            vote_count=response['results'][i]['vote_count']
+        except:
+            vote_count="N/A"
+        try:
+            genre_ids=response['results'][i]['genre_ids']
+        except:
+            genre_ids="N/A"
+        results.append({"id":id,
+                        "title":title,
+                        "overview":overview,
+                        "poster_path":poster_path,
+                        "release_date":release_date,
+                        "vote_average":vote_average,
+                        "vote_count":vote_count,
+                        "genre_ids":genre_ids})
     return {"results":results}
 
 app.run(debug=True)
