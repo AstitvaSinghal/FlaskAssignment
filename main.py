07f375710c16c791ee2830443f2da9cf533c236b
singhalswastik <swastik@iitk.ac.in>
2021-03-09 00:51:51 +0530

    movie details

--- main.py
+++ main.py
@@ -104,7 +104,6 @@ def search_movie():
 @app.route('/search_tv')
 def search_tv():
     search_query=request.args.get('search_query')
-    print("check")
     response=get(f'{SITE_NAME}search/tv?{API_KEY}&language=en-US&page=1&query={search_query}&include_adult=false')
     response=response.json()
     results=[]
@@ -154,7 +153,6 @@ def search_tv():
 @app.route('/search_multi')
 def search_multi():
     search_query=request.args.get('search_query')
-    print("check")
     response=get(f'{SITE_NAME}search/multi?{API_KEY}&language=en-US&page=1&query={search_query}&include_adult=false')
     response=response.json()
     results=[]
@@ -219,6 +217,63 @@ def search_multi():
                             "vote_count":vote_count,
                             "genre_ids":genre_ids})
     return {"results":results}
+
+@app.route('/movie')
+def movie_details():
+    movie_id=request.args.get('movie_id')
+    response=get(f'{SITE_NAME}movie/{movie_id}?{API_KEY}&language=en-US')
+    response=response.json()
+    try:
+        id=response['id']
+    except:
+        id="N/A"
+    try:
+        title=response['title']
+    except:
+        title="N/A"
+    try:
+        runtime=response['runtime']
+    except:
+        runtime="N/A"
+    try:
+        release_date=response['release_date']
+    except:
+        release_date="N/A"
+    try:
+        spoken_languages=response['spoken_languages']
+    except:
+        spoken_languages="N/A"
+    try:
+        vote_average=response['vote_average']
+    except:
+        vote_average="N/A"
+    try:
+        vote_count=response['vote_count']
+    except:
+        vote_count="N/A"
+    try:
+        poster_path=response['poster_path']
+    except:
+        poster_path="N/A"
+    try:
+        genres=response['genres']
+    except:
+        genres="N/A"
+    try:
+        backdrop_path=response['backdrop_path']
+    except:
+        backdrop_path="N/A"
+    results={"id":id,
+                    "title":title,
+                    "runtime":runtime,
+                    "release_date":release_date,
+                    "spoken_languages":spoken_languages,
+                    "vote_average":vote_average,
+                    "vote_count":vote_count,
+                    "poster_path":poster_path,
+                    "backdrop_path":backdrop_path,
+                    "genres":genres}
+    return results
 
 
 app.run(debug=True)
