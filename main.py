d51d5e58dd61058fc902a28c785da6d45e2bbd34
singhalswastik <swastik@iitk.ac.in>
2021-03-08 23:54:31 +0530

    airing

--- main.py
+++ main.py
@@ -11,8 +11,46 @@ def serveHomePage():
     return app.send_static_file('index.html')
 
 @app.route('/trending')
-def proxy():
+def trending():
     response=get(f'{SITE_NAME}trending/movie/week?{API_KEY}')
-    app.logger.info(len(response.json()['results']))
-    return response.json()
+    response=response.json()
+    results=[]
+    for i in range(0,5):
+        try:
+            title=response['results'][i]['title']
+        except:
+            title="N/A"
+        try:
+            backdrop_path=response['results'][i]['backdrop_path']
+        except:
+            backdrop_path="N/A"
+        try:
+            release_date=response['results'][i]['release_date']
+        except:
+            release_date="N/A"
+        results.append({"title":title,"backdrop_path":backdrop_path,"release_date":release_date})
+    return {"results":results}
+
+@app.route('/airing_today')
+def airing_today():
+    response=get(f'{SITE_NAME}tv/airing_today?{API_KEY}')
+    response=response.json()
+    # return response
+    results=[]
+    for i in range(0,5):
+        try:
+            name=response['results'][i]['name']
+        except:
+            name="N/A"
+        try:
+            backdrop_path=response['results'][i]['backdrop_path']
+        except:
+            backdrop_path="N/A"
+        try:
+            first_air_date=response['results'][i]['first_air_date']
+        except:
+            first_air_date="N/A"
+        results.append({"name":name,"backdrop_path":backdrop_path,"first_air_date":first_air_date})
+    return {"results":results}
+
 app.run(debug=True)
